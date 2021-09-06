6c7d530b69f70a5cbdb6c3006bf459982547101f
GRandAMi1 <Amitshendge1990@gmail.com>
2021-09-06 16:46:32 +0530

    Added method to edit contact

--- AddressBook/src/AddressBook.java
+++ AddressBook/src/AddressBook.java
@@ -2,15 +2,20 @@ public class AddressBook {
     public static void main(String[] args) {
         AddressBookOperations operations = new AddressBookOperations();
         UserInputUtil input = new UserInputUtil();
+        int a = 0;
+            System.out.println("1. add\n2. display\n3. edit Contact\n0.exit");
+        while (a == 0) {
+            System.out.print("\nEnter your choice : ");
 
-        while (true) {
-            System.out.println("1. add\n2. display");
-            System.out.println("Enter your choice : ");
-            if (input.intInput() == 1) {
-                operations.add();
-            }
-            else {
-                operations.display();break;
+            switch (input.intInput()) {
+                case 1 -> operations.add();
+                case 2 -> operations.display();
+                case 3 -> operations.edit();
+                case 0 -> {
+                    a = 1;
+                    System.out.println("Thank you for your Time");
+                }
+                default -> System.out.println("Choose only from options");
             }
         }
     }
